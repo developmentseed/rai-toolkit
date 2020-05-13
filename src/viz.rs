4abe29bc0ff57f12936590631ccdc92f6b19076d
ingalls <nick@ingalls.ca>
2020-05-13 11:54:50 -0700

    Add extent calc

--- src/viz.rs
+++ src/viz.rs
@@ -2,8 +2,11 @@ use serde_json::json;
 use crate::mvt;
 use actix_web::{web, App, HttpResponse, HttpServer, middleware, web::Json};
 
+#[derive(Debug, Clone)]
+struct Country(String);
+
 pub fn main(pool: r2d2::Pool<r2d2_postgres::PostgresConnectionManager<postgres::NoTls>>, args: &clap_v3::ArgMatches) {
-    let iso = args.value_of("iso").unwrap().to_string().to_lowercase();
+    let iso = Country(args.value_of("iso").unwrap().to_string().to_lowercase());
 
     let token = match std::env::var("MAPBOX_TOKEN") {
         Ok(tk) => tk,
@@ -20,6 +23,7 @@ pub fn main(pool: r2d2::Pool<r2d2_postgres::PostgresConnectionManager<postgres::
             .wrap(middleware::Compress::default())
             .data(pool.clone())
             .data(token.clone())
+            .data(iso.clone())
             .service(web::scope("tiles")
                 .service(web::resource("")
                     .route(web::get().to(map_get))
@@ -41,9 +45,26 @@ pub fn main(pool: r2d2::Pool<r2d2_postgres::PostgresConnectionManager<postgres::
 }
 
 fn map_get(
+    db: web::Data<r2d2::Pool<r2d2_postgres::PostgresConnectionManager<postgres::NoTls>>>,
+    iso: web::Data<Country>,
     token: web::Data<String>
 ) -> Json<serde_json::Value> {
+
+    let extent = match db.get().unwrap().query(format!(r#"
+        SELECT
+            regexp_replace(regexp_replace(regexp_replace(ST_Extent(geom)::TEXT, 'BOX\(', '['), '\)', ']'), ' ', ',', 'g')::JSON
+        FROM
+            country_{iso}.{iso}_geom;
+    "#, iso = &iso.0).as_str(), &[]) {
+        Err(err) => panic!(err.to_string()),
+        Ok(rows) => {
+            let bounds: serde_json::Value = rows.get(0).unwrap().get(0);
+            bounds
+        }
+    };
+
     Json(json!({
+        "bounds": extent,
         "token": token.as_str()
     }))
 }
